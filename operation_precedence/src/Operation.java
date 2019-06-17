2ee0026658311301d681556c0c78bef4a3d196fb
Junenatte-and-little-pig <sy5622_5@126.com>
2019-06-17 10:23:35 +0800

    finish operation precedence

--- operation_precedence/src/Operation.java
+++ operation_precedence/src/Operation.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Set;
 
 public class Operation {
@@ -6,19 +7,24 @@ public class Operation {
         Set<Character> nonTerminal = r.getNonTerminal();
         System.out.println("firstVT集");
         for (Character c : nonTerminal) {
-            System.out.print(c+":{\t");
+            System.out.print(c + ":{\t");
             for (Character ch : r.getFirstVTs().get(c))
                 System.out.print(ch + "\t");
             System.out.println("}");
         }
         System.out.println("lastVT集");
         for (Character c : nonTerminal) {
-            System.out.print(c+":{\t");
-            for (Character ch : r.getlastVTs().get(c))
+            System.out.print(c + ":{\t");
+            for (Character ch : r.getLastVTs().get(c))
                 System.out.print(ch + "\t");
             System.out.println("}");
         }
-        //r.printTable();
-        r.findUsages();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (input.indexOf('#') != input.length() - 1) {
+            System.out.println("请正确的以#结尾");
+            input = sc.nextLine();
+        }
+        r.analyse(input);
     }
 }
--- operation_precedence/src/Rule.java
+++ operation_precedence/src/Rule.java
@@ -1,5 +1,15 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
 class Rule {
     private Map<Character, List<String>> rules;
@@ -8,6 +18,7 @@ class Rule {
     private Map<Character, Boolean> toEmpty;
     private Map<Character, Set<Character>> firstVTs;
     private Map<Character, Set<Character>> lastVTs;
+    private Map<Character, Map<Character, Character>> OPTable;
 
     Rule(String path) {
         rules = new HashMap<>();
@@ -16,6 +27,7 @@ class Rule {
         toEmpty = new HashMap<>();
         firstVTs = new HashMap<>();
         lastVTs = new HashMap<>();
+        OPTable = new HashMap<>();
         try {
             File f = new File(path);
             BufferedReader br = new BufferedReader(new FileReader(f));
@@ -24,6 +36,7 @@ class Rule {
                 line = br.readLine();
                 if (line == null)
                     break;
+                System.out.println(line);
                 String[] ls = line.split("->");
                 String[] rs = ls[1].split("\\|");
                 List<String> r = new ArrayList<>(Arrays.asList(rs));
@@ -38,34 +51,29 @@ class Rule {
                         continue;
                     }
                     for (char ch : str.toCharArray())
-                        if (!nonTerminal.contains(ch) && ch != '@')
+                        if (!nonTerminal.contains(ch) && ch != '@') {
                             terminal.add(ch);
+                            OPTable.put(ch, new HashMap<>());
+                        }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        initFirstsVT();
-        initFollowsVT();
-    }
-
-    Map<Character, List<String>> getRules() {
-        return rules;
+        initFirstVT();
+        initLastVT();
+        generateTable();
     }
 
     Set<Character> getNonTerminal() {
         return nonTerminal;
     }
 
-    Set<Character> getTerminal() {
-        return terminal;
-    }
-
     Map<Character, Set<Character>> getFirstVTs() {
         return firstVTs;
     }
 
-    Map<Character, Set<Character>> getlastVTs() {
+    Map<Character, Set<Character>> getLastVTs() {
         return lastVTs;
     }
 
@@ -99,28 +107,7 @@ class Rule {
         firstVTs.put(nonT, firstVT);
     }
 
-    private Set<Character> getFirstVTByRule(String rule) {
-        Set<Character> firstVT = new HashSet<>();
-        int i = 0;
-        while (true) {
-            Character c = rule.charAt(i);
-            if (nonTerminal.contains(c)) {
-                firstVT.addAll(firstVTs.get(c));
-                if (toEmpty.get(c))
-                    i++;
-                else
-                    break;
-            } else if (terminal.contains(c)) {
-                firstVT.add(c);
-                break;
-            } else if (c.equals('@')) {
-                break;
-            }
-        }
-        return firstVT;
-    }
-
-    private void getlastVT(Character nonT) {
+    private void getLastVT(Character nonT) {
         Set<Character> lastVT = new HashSet<>();
         List<String> rule = rules.get(nonT);
         for (String s : rule) {
@@ -143,7 +130,7 @@ class Rule {
         lastVTs.put(nonT, lastVT);
     }
 
-    private void initFirstsVT() {
+    private void initFirstVT() {
         boolean changed = true;
         while (changed) {
             changed = false;
@@ -158,47 +145,147 @@ class Rule {
         }
     }
 
-    private void initFollowsVT() {
+    private void initLastVT() {
         boolean changed = true;
         while (changed) {
             changed = false;
             int length = getLength(lastVTs);
             for (Character c : nonTerminal)
-                getlastVT(c);
+                getLastVT(c);
             if (length < getLength(lastVTs))
                 changed = true;
         }
     }
 
-    void findUsages() {
+    private Character findUsage(Character ch) {
         for (Character c : nonTerminal)
             for (String s : rules.get(c))
-                for (Character ch : s.toCharArray())
-                    if (terminal.contains(ch))
-                        System.out.println(ch + " in " + s);
+                if (s.indexOf(ch) >= 0)
+                    return c;
+        return null;
     }
 
-    void printTable() {
+    private int findUsageLength(Character ch) {
+        for (Character c : nonTerminal)
+            for (String s : rules.get(c))
+                if (s.indexOf(ch) >= 0)
+                    return s.length();
+        return 0;
+    }
+
+    private Character rollback(Character ch) {
+        if (ch == 'E')
+            return 'E';
+        for (Character c : nonTerminal)
+            for (String s : rules.get(c))
+                if (s.length() == 1 && s.indexOf(ch) == 0)
+                    return rollback(c);
+        return ch;
+    }
+
+    private void generateTable() {
         for (Character c : nonTerminal)
             for (String s : rules.get(c))
                 for (Character ch : s.toCharArray())
                     if (terminal.contains(ch)) {
                         int index = s.indexOf(ch);
                         if (index + 1 < s.length()) {
-                            char after = s.charAt(index - 1);
+                            char after = s.charAt(index + 1);
                             if (nonTerminal.contains(after)) {
-                                for(Character first:firstVTs.get(after))
-                                    System.out.println(ch+"<"+first);
-                                if(index+2<s.length()&&terminal.contains(s.charAt(index+2)))
-                                    System.out.println(ch+"="+after);
-                            } else if (terminal.contains(after))
-                                System.out.println(ch+"="+after);
+                                for (Character first : firstVTs.get(after)) {
+                                    Map<Character, Character> temp = OPTable.get(ch);
+                                    temp.put(first, '<');
+                                    OPTable.put(ch, temp);
+                                }
+                                if (index + 2 < s.length() && terminal.contains(s.charAt(index + 2))) {
+                                    after = s.charAt(index + 2);
+                                    if (terminal.contains(after)) {
+                                        Map<Character, Character> temp = OPTable.get(ch);
+                                        temp.put(after, '=');
+                                        OPTable.put(ch, temp);
+                                    }
+                                }
+                            } else if (terminal.contains(after)) {
+                                Map<Character, Character> temp = OPTable.get(ch);
+                                temp.put(after, '=');
+                                OPTable.put(ch, temp);
+                            }
                         }
-                    }else if(nonTerminal.contains(ch)){
-                        int index=s.indexOf(ch);
-                        if(index+1<s.length()&&terminal.contains(s.charAt(index+1)))
-                            for(Character last:lastVTs.get(ch))
-                                System.out.println(last+">"+ch);
+                    } else if (nonTerminal.contains(ch)) {
+                        int index = s.indexOf(ch);
+                        if (index + 1 < s.length() && terminal.contains(s.charAt(index + 1)))
+                            for (Character last : lastVTs.get(ch)) {
+                                Map<Character, Character> temp = OPTable.get(last);
+                                temp.put(s.charAt(index + 1), '>');
+                                OPTable.put(last, temp);
+                            }
                     }
+        OPTable.put('#', new HashMap<>());
+        for (Character c : terminal) {
+            Map<Character, Character> m = OPTable.get(c);
+            m.put('#', '>');
+            OPTable.put(c, m);
+            m = OPTable.get('#');
+            m.put(c, '<');
+            OPTable.put('#', m);
+        }
+        Map<Character, Character> m = OPTable.get('#');
+        m.put('#', '=');
+        OPTable.put('#', m);
+    }
+
+    void analyse(String input) {
+        Stack<Character> state = new Stack<>();
+        state.push('#');
+        int top = state.size() - 1;
+        for (int index = 0; index < input.length(); index++) {
+            Character c = input.charAt(index);
+            if (c != '#' && !terminal.contains(c)) {
+                System.out.println("非标准语法");
+                return;
+            }
+            if (OPTable.get(state.get(top)).get(c) == '<') {
+                state.push(c);
+                top = state.size() - 1;
+            } else if (OPTable.get(state.get(top)).get(c) == '=')
+                state.push(c);
+            else if (OPTable.get(state.get(top)).get(c) == '>') {
+                Character re = findUsage(state.get(top));
+                int pos;
+                while (true) {
+                    if (state.size() == 1)
+                        break;
+                    if (nonTerminal.contains(state.peek()))
+
+                        top = state.size() - 2;
+                    else
+                        top = state.size() - 1;
+                    pos = top - 1;
+                    if (!terminal.contains(state.get(pos)) && state.get(pos) != '#')
+                        pos--;
+                    Character tc = state.get(top);
+                    Character pc = state.get(pos);
+                    int l = findUsageLength(tc);
+                    if (l == state.size() - pos - 1)
+                        for (int i = 0; i < l; i++)
+                            state.pop();
+                    else {
+                        System.out.println("ERROR");
+                        return;
+                    }
+                    if (OPTable.get(pc).get(tc) == '<')
+                        break;
+                }
+                state.push(re);
+                index--;
+                top = state.size() - 2;
+            } else {
+                System.out.println("ERROR");
+            }
+        }
+        if (state.size() == 3 && rollback(state.get(1)) == 'E')
+            System.out.println("accept");
+        else
+            System.out.println("decline");
     }
 }
